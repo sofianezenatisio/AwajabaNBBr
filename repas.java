95be1a951ef21c209d63372f816461d98089d2b4
sofianezenatisio <sofiane.zenati.sio@gmail.com>
2026-09-07 11:08:35 +0200

    C3

--- repas.java
+++ repas.java
@@ -3,6 +3,12 @@ public class Repas {
 	private int numero;
 	private LocalDate date ;
 	
+	public Repas(int numero,LocalDate date){
+		super();
+		this.numero = numero;
+		this.date = date ;
+	}
+		
 	
 	
 	
